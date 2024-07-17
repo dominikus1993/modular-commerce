9c55d10903a074bb4db4228be49d16cfc8d70e4c
Dominik.Kotecki <dominikus1910@gmail.com>
2024-07-17 18:22:41 +0200

    add some data

--- src/Modules/Catalog/Setup.cs
+++ src/Modules/Catalog/Setup.cs
@@ -53,6 +53,16 @@ public static class Setup
             new AvailableQuantity(20), [new Tag("soap"), new Tag("body")]);
         yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce220")), new ProductName("colgate"), new ProductDescription("colgate toothpaste"), new ProductPrice(new Price(3m), new Price(1m)), new AvailableQuantity(30),
             [new Tag("toothpaste"), new Tag("teeth")]);
-        
+        yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce221")), new ProductName("dettol"), new ProductDescription("dettol handwash"), new ProductPrice(new Price(4m), new Price(2m)), new AvailableQuantity(40),
+            [new Tag("handwash"), new Tag("hands")]);
+        yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce222")), new ProductName("tide"), new ProductDescription("tide detergent"), new ProductPrice(new Price(8m), new Price(4m)), new AvailableQuantity(50),
+            [new Tag("detergent"), new Tag("clothes")]);
+        yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce223")), new ProductName("pepsi"), new ProductDescription("pepsi drink"), new ProductPrice(new Price(2m), new Price(1m)), new AvailableQuantity(60),
+            [new Tag("drink"), new Tag("beverage")]);
+        yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce224")), new ProductName("lays"), new ProductDescription("lays chips"), new ProductPrice(new Price(1m), new Price(0.5m)), new AvailableQuantity(70),
+            [new Tag("chips"), new Tag("snacks")]);
+        yield return new Product(new ProductId(new Guid("4c735c18-dcbf-4e72-b479-fa1e36dce225")), new ProductName("kitkat"), new ProductDescription("kitkat chocolate"), new ProductPrice(new Price(3m), new Price(1.5m)), new AvailableQuantity(80),
+            [new Tag("snacks"), new Tag("chocolate")]);
+
     }
 }
