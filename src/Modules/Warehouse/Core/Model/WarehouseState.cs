8e8956670c03201a05d8fd63d3f1945ba1a8edb0
Dominik.Kotecki <dominikus1910@gmail.com>
2024-08-27 17:56:25 +0200

    add model

---
+++ src/Modules/Warehouse/Core/Model/WarehouseState.cs
@@ -0,0 +1,36 @@
+namespace Warehouse.Core.Model;
+
+public readonly record struct ItemWarehouseQuantity(int Value)
+{
+    public static implicit operator ItemWarehouseQuantity(int value) => new ItemWarehouseQuantity(value);
+    
+    public static implicit operator int(ItemWarehouseQuantity value) => value.Value;
+}
+
+public readonly record struct ItemSoldQuantity(int Value)
+{
+    public static implicit operator ItemSoldQuantity(int value) => new ItemSoldQuantity(value);
+    
+    public static implicit operator int(ItemSoldQuantity value) => value.Value;
+}
+
+public readonly record struct ItemReservedQuantity(int Value)
+{
+    public static implicit operator ItemReservedQuantity(int value) => new ItemReservedQuantity(value);
+    
+    public static implicit operator int(ItemReservedQuantity value) => value.Value;
+}
+
+public sealed record ItemAvailability(ItemWarehouseQuantity WarehouseQuantity, ItemSoldQuantity SoldQuantity, ItemReservedQuantity ReservedQuantity);
+public readonly record struct ItemId(Guid Value)
+{
+    public static ItemId From(Guid id) => new ItemId(id);
+    
+    public static ItemId New() => new ItemId(Guid.NewGuid());
+    
+    public static implicit operator Guid (ItemId id) => id.Value;
+    
+    public static implicit operator ItemId (Guid id) => new ItemId(id);
+}
+
+public sealed record WarehouseState(ItemId ItemId, ItemAvailability Availability);
